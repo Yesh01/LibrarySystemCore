916e279c5b4b37dc277b172601a5ae0cabbc4d40
Yearu <yeshuavenezuela0@gmail.com>
2024-09-14 07:24:34 +0800

    Creating Package & Filing

---
+++ src/lms/LibraryManagementSystem.java
@@ -0,0 +1,11 @@
+package com.jts.lms;
+
+public class LibraryManagementSystem {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Welcome");
+
+        System.out.println("Log-in First.");
+    }
+}
