a36ff03df6b35f015ad4a682aa7c8f8114a09689
Yearu <yeshuavenezuela0@gmail.com>
2024-09-27 14:24:54 +0800

    Issue FIxed of Text Format Displayed

--- src/lms/SystemManagement.java
+++ src/lms/SystemManagement.java
@@ -4,9 +4,13 @@ import java.sql.SQLException;
 import lms.login.LoginService;
 
 public class SystemManagement {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        System.out.println("------ Welcome to Universitiy's Library! ------");
-        System.out.println("[ :> ] Plase do Login --> ");
+    public static void main(String[] args) throws ClassNotFoundException, SQLException { 
+
+        System.out.println("                                               ");
+        System.out.println("   --- Welcome to Universitiy's Library! ---   ");
+        System.out.println("                                        ");
+
+        System.out.println("[ :> ] Please do Login -->    ");
 
         LoginService loginService = new LoginService();
         loginService.doLogin();
