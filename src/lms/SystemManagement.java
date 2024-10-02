aeb1e38619e7cfe16f76dd7288ef123b35c15352
Yearu <yeshuavenezuela0@gmail.com>
2024-10-02 22:55:21 +0800

    [Finished] Small Project of this one is @Temporary Finished! There was a lot ideas of adjustments to impove and enhancement in every area of this source code but overall, the objectives of this small project for learning phase through building a project is already accomplished. Project is Temporary considered done.

--- src/lms/SystemManagement.java
+++ src/lms/SystemManagement.java
@@ -7,7 +7,7 @@ public class SystemManagement {
     public static void main(String[] args) throws ClassNotFoundException, SQLException { 
 
         System.out.println("                                               ");
-        System.out.println("   --- Welcome to University's Library! ---   ");
+        System.out.println("   ---- Welcome to University's Library! ----   ");
         System.out.println("                                        ");
 
         System.out.println("[ :> ] Please do Login -->    ");
@@ -16,3 +16,9 @@ public class SystemManagement {
         loginService.doLogin();
     }
 }
+
+// @Finished [Small Project][Learning Built-In Project]
+// @Temporary Done
+// @Yesh01 --> Author
+
+
